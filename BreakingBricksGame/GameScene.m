6bd880f75f5b811d70de6513368ff600aa40b342
Varun Rathi <varunrathi28@gmail.com>
2017-05-26 03:27:55 +0530

    Added Paddle for the player

--- BreakingBricksGame/GameScene.m
+++ BreakingBricksGame/GameScene.m
@@ -8,6 +8,16 @@
 
 #import "GameScene.h"
 
+#define kPlayerPositionHeight 100.0
+
+#define kScreenWidth [[[[UIScreen mainScreen] bounds] size] width]
+
+@interface GameScene()
+
+@property (nonatomic) SKSpriteNode * paddleNode;
+
+@end
+
 @implementation GameScene {
 }
 
@@ -22,27 +32,70 @@
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     self.physicsWorld.gravity = CGVectorMake(0,0);
     self.backgroundColor = [SKColor whiteColor];
-    SKSpriteNode * ballNode = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
-    ballNode.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    
-    
-    ballNode.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ballNode.frame.size.height/2];
-    ballNode.physicsBody.dynamic = YES;
-    
-    
-    [self addChild:ballNode];
-    
-    CGVector myVector = CGVectorMake(10, 50);
-    [ballNode.physicsBody applyImpulse:myVector];
-    ballNode.physicsBody.linearDamping = 0;
-    ballNode.physicsBody.restitution = 1;
-    ballNode.physicsBody.friction = 0;
-    
+  
+    [self addBallNodeWithSize:size];
+    [self addPlayer:size];
     return self;
     
 }
 
 
+-(void)addBallNodeWithSize:(CGSize)size
+{
+    SKSpriteNode * ballNode = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    ballNode.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    
+    
+    ballNode.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ballNode.frame.size.height/2];
+    
+    
+    
+    [self addChild:ballNode];
+    
+    CGVector myVector = CGVectorMake(20, 20);
+    [ballNode.physicsBody applyImpulse:myVector];
+    ballNode.physicsBody.linearDamping = 0;
+    ballNode.physicsBody.restitution = 1;
+    ballNode.physicsBody.friction = 0;
+    
+}
+
+
+-(void)addPlayer:(CGSize )size
+{
+     self.paddleNode = [SKSpriteNode spriteNodeWithImageNamed:@"paddle"];
+    _paddleNode.position = CGPointMake(self.frame.size.width/2, kPlayerPositionHeight);
+    self.paddleNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddleNode.frame.size];
+    self.paddleNode.physicsBody.affectedByGravity = NO;
+    
+    self.paddleNode.physicsBody.dynamic = NO;
+    [self addChild:_paddleNode];
+}
+
+
+
+
+-(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
+    for (UITouch * touch in touches)
+    {
+        CGPoint location = [touch locationInNode:self];
+        CGPoint newPaddlePos = CGPointMake(location.x, kPlayerPositionHeight);
+        
+        if(newPaddlePos.x < self.paddleNode.size.width/2)
+        {
+            newPaddlePos.x = self.paddleNode.size.width/2;
+        }
+        
+        else if (newPaddlePos.x >self.frame.size.width - self.paddleNode.size.width/2)
+        {
+            newPaddlePos.x = self.frame.size.width - self.paddleNode.size.width/2;
+        }
+        
+        self.paddleNode.position = newPaddlePos;
+    }
+}
 
 -(void)update:(CFTimeInterval)currentTime {
     // Called before each frame is rendered
