bbf614a640634a21a0376dd36b256b68c38f131b
Varun Rathi <varunrathi28@gmail.com>
2017-05-26 01:33:35 +0530

    Physics body added to the ball

--- BreakingBricksGame/GameScene.m
+++ BreakingBricksGame/GameScene.m
@@ -19,9 +19,17 @@
 {
     self =[super initWithSize:size];
     
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    self.physicsWorld.gravity = CGVectorMake(0, -1.6);
     self.backgroundColor = [SKColor whiteColor];
     SKSpriteNode * ballNode = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
     ballNode.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    
+    
+    ballNode.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ballNode.frame.size.height/2];
+    ballNode.physicsBody.dynamic = YES;
+    
+    
     [self addChild:ballNode];
     
     return self;
